aeb224f3eb5cddf74d07d1e3170690d415567cfa
Tiago Carbone do Nascimento <carbonetiago@id.uff.br>
2023-09-26 18:38:56 -0300

    atualizacoes finais

--- src/data/dataFake.ts
+++ src/data/dataFake.ts
@@ -8,15 +8,23 @@ export const dataFake = [
     {
         "id": '1',
         "photo": "aaa",
-        "title": "saiu novo filme",
-        "description": "oiiiii",
+        "title": "Faz mto gol",
+        "description": "Faz o L aig",
         "photoCover": "https://tntsports.com.br/__export/1693538222141/sites/esporteinterativo/img/2023/09/01/german_cano.jpeg_423682103.jpeg"
     }, {
         "id": '2',
         "photo": "aaa",
         "title": "nenseeeee",
         "description": "ffcccccccccc",
-        "photoCover": "ffc"
+        "photoCover": "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltb315b96e4ece79ed/62c9fb498c8b0743ca32feaf/51696197183_060d6e97c2_o.jpg"
+    },
+    
+    {
+        "id" : "3",
+        "photo" : "ffc",
+        "title" : "Dario Conca é recordista de assistências em uma única edição de campeonato brasileiro",
+        "description" : "Com 19 assistências..... nenseeeeeeee",
+        "photoCover": "https://cdn.conmebol.com/wp-content/uploads/2015/01/conca.jpg"
     }
 
 ]
